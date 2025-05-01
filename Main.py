0e203e53e025cbc054463e82a7d2bfc951272d34
Qobixx <127144254+Qobixx@users.noreply.github.com>
2025-05-01 22:43:28 +0200

    Update Wetter 1.0

--- Main.py
+++ Main.py
@@ -14,6 +14,7 @@ load_dotenv()
 # Hole den Token aus der Umgebungsvariable
 TOKEN = os.getenv("DISCORD_TOKEN")
 WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 # Wetterdaten für 4 Tage abrufen
